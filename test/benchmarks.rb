500126629851ebb9dd1941ebec834f88fab1c9df
Ben Dunkley <ben@chemica.co.uk>
2014-02-23 18:08:54 +0000

    Small changes to benchmarks

--- test/benchmarks.rb
+++ test/benchmarks.rb
@@ -4,16 +4,16 @@ require 'benchmark'
 require 'recursive-open-struct'
 
 def benchmarks
-  puts "Benchmarking 10000 operations on deep hash:"
+  puts 'Benchmarking 10000 operations on deep hash:'
 
   hash = {:aaaaBbbb => 'ccccDDDD',
           :a => 2, :b => 2, :c => 2, :d => 2, :e => 2,
           :f => 2, :g => 2, :h => 2, :i => 2, :j => 2,
           :k => 2, :l => 2, :m => 2, :n => 2, :o => 2,
           :p => 2, :q => 2, :r => 2, :s => 2, :t => 2,
-          'eeeeEeee' =>[{"NameThe1" => {"CamelName" => 4444},
-                         "NameThe3" => [4,5,6]}, "3", "4"]}
-  puts "whoopee-cushion"
+          'eeeeEeee' =>[{'NameThe1' => {'CamelName' => 4444},
+                         'NameThe3' => [4,5,6]}, '3', '4']}
+  puts 'whoopee-cushion'
   puts Benchmark.measure {
     10000.times do
       a = WhoopeeCushion::Inflate.from_hash hash, :convert_keys => false
@@ -21,7 +21,7 @@ def benchmarks
     end
   }
 
-  puts "whoopee-cushion with camel to snake case conversion"
+  puts 'whoopee-cushion with camel to snake case conversion'
   puts Benchmark.measure {
     10000.times do
       a = WhoopeeCushion::Inflate.from_hash hash
@@ -37,36 +37,35 @@ def benchmarks
     end
   }
 
-
-
-  puts "Benchmarking 10000 operations on light hash:"
+  puts ''
+  puts 'Benchmarking 10000 whoopee-cushion conversions of a shallow hash:'
 
   hash = {:a => 2, :b => 2, :c => 2, :d => 2, :e => 2,
           :f => 2, :g => 2, :h => 2, :i => 2, :j => 2,
-          :k => 2, :l => 2, :m => 2, :n => 2, :c1 => "Lorem Ipsum"}
+          :k => 2, :l => 2, :m => 2, :n => 2, :camelBackCase => "Lorem Ipsum"}
 
 
   puts Benchmark.measure {
     10000.times do
       x = WhoopeeCushion::Inflate.from_hash hash, :convert_keys => false
-      x.c1
+      x.camelBackCase
     end
   }
 
-  puts "Benchmarking 10000 operations on light hash with camel case conversion:"
+  puts 'Benchmarking 10000 whoopee-cushion conversions of a shallow hash with camel case conversion:'
   puts Benchmark.measure {
     10000.times do
       x = WhoopeeCushion::Inflate.from_hash hash
-      x.c1
+      x.camel_back_case
     end
   }
 
-  puts "Benchmarking 10000 OpenStruct creates"
+  puts "Benchmarking 10000 OpenStruct creates with a shallow hash"
 
   puts Benchmark.measure {
     10000.times do
       x = OpenStruct.new(hash)
-      x.c1
+      x.camelBackCase
     end
   }
 end
